3adaa541d4c411b88e2a6ede2d22848f62e6d2f5
AlejandroGomezUAI <30306375+AlejandroGomezUAI@users.noreply.github.com>
2018-02-08 16:38:20 -0300

    frmProfesor + usercontrols

---
+++ SIUNCA/GUI/frmProfesor.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Windows.Forms;
+using GUI.UserControlProfesor;
+
+namespace GUI
+{
+    public partial class frmProfesor : Form
+    {
+        //variables para la funcion arrastrarForm(e, x, y)
+        int x = 0, y = 0;
+
+        //instancias necesarias para el metodo seleccionaUserontrol(UserControl)
+        UCAsistencia ucasistencia = new UCAsistencia();
+        UCNotas ucnotas = new UCNotas();
+
+        public frmProfesor()
+        {
+            InitializeComponent();
+            seleccionarUserControl(ucasistencia);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMaximizar_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            else
+            {
+            this.WindowState = FormWindowState.Maximized;
+            }
+        }
+
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+
+        private void btnAsistencia_Click(object sender, EventArgs e)
+        {
+            
+            moverSlider(btnAsistencia.Height, btnAsistencia.Top);
+           
+            seleccionarUserControl(ucasistencia);
+        }
+
+        private void btnNotas_Click(object sender, EventArgs e)
+        {
+            
+            moverSlider(btnNotas.Height, btnNotas.Top);
+            
+            seleccionarUserControl(ucnotas);
+        }
+
+
+
+        private void panelHeader2_MouseMove(object sender, MouseEventArgs e)
+        {
+            arrastrarForm(e, ref x, ref y);
+        }
+
+        private void panelMenu_MouseMove(object sender, MouseEventArgs e)
+        {
+            arrastrarForm(e, ref x, ref y);
+        }
+        
+
+        private void panelHeader1_MouseMove(object sender, MouseEventArgs e)
+        {
+            arrastrarForm(e,ref x, ref y);
+        }
+
+        /// <summary>
+        /// Metodo para desilzar slider sobre el menu
+        /// </summary>
+        /// <param name="height">parametro de la altura del boton</param>
+        /// <param name="top">parametro para la el tope del boton</param>
+        public void moverSlider(int height, int top)
+        {
+            ////codigo para selector de menu///
+            slideMenu.Height = height;
+            slideMenu.Top = top;
+        }
+
+
+        /// <summary>
+        /// Metodo para arrastrar el form desde el menu lateral y el header
+        /// </summary>
+        /// <param name="e">evento MouseEventArgs</param>
+        /// <param name="x">valor por referencia del eje horizontal</param>
+        /// <param name="y">valor por referencia del eje vertical</param>
+        public void arrastrarForm( MouseEventArgs e, ref int x, ref int y)
+        {
+            if (e.Button != MouseButtons.Left)
+            {
+                x = e.X;
+                y = e.Y;
+            }
+            else
+            {
+                Left = Left + (e.X - x);
+                Top = Top + (e.Y - y);
+            }
+        }
+
+        /// <summary>
+        /// metodo para traer al panelcontenedor el usercontrol seleccionado en
+        /// el menu lateral
+        /// </summary>
+        /// <param name="uc">instancia de un usercontrol</param>
+        public void seleccionarUserControl(UserControl uc)
+        {
+            panelContenedor.Controls.Clear();
+            panelContenedor.Controls.Add(uc);
+        }
+    }
+}
---
+++ SIUNCA/GUI/frmProfesor.designer.cs
@@ -0,0 +1,266 @@
+﻿using GUI.UserControlProfesor;
+
+namespace GUI
+{
+    partial class frmProfesor
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmProfesor));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.slideMenu = new System.Windows.Forms.Panel();
+            this.btnNotas = new System.Windows.Forms.Button();
+            this.btnAsistencia = new System.Windows.Forms.Button();
+            this.panelHeader1 = new System.Windows.Forms.Panel();
+            this.btnMinimizar = new System.Windows.Forms.Button();
+            this.btnMaximizar = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panelHeader2 = new System.Windows.Forms.Panel();
+            this.lblNombreProfesor = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panelContenedor = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panelHeader1.SuspendLayout();
+            this.panelHeader2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(82)))), ((int)(((byte)(204)))));
+            this.panelMenu.Controls.Add(this.label1);
+            this.panelMenu.Controls.Add(this.slideMenu);
+            this.panelMenu.Controls.Add(this.btnNotas);
+            this.panelMenu.Controls.Add(this.btnAsistencia);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(203, 513);
+            this.panelMenu.TabIndex = 0;
+            this.panelMenu.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelMenu_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(39, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SIUNCA";
+            // 
+            // slideMenu
+            // 
+            this.slideMenu.BackColor = System.Drawing.Color.Black;
+            this.slideMenu.Location = new System.Drawing.Point(188, 123);
+            this.slideMenu.Name = "slideMenu";
+            this.slideMenu.Size = new System.Drawing.Size(15, 44);
+            this.slideMenu.TabIndex = 2;
+            // 
+            // btnNotas
+            // 
+            this.btnNotas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.btnNotas.FlatAppearance.BorderSize = 0;
+            this.btnNotas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNotas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNotas.Location = new System.Drawing.Point(0, 173);
+            this.btnNotas.Name = "btnNotas";
+            this.btnNotas.Size = new System.Drawing.Size(188, 44);
+            this.btnNotas.TabIndex = 4;
+            this.btnNotas.Text = "Notas";
+            this.btnNotas.UseVisualStyleBackColor = false;
+            this.btnNotas.Click += new System.EventHandler(this.btnNotas_Click);
+            // 
+            // btnAsistencia
+            // 
+            this.btnAsistencia.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.btnAsistencia.FlatAppearance.BorderSize = 0;
+            this.btnAsistencia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAsistencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAsistencia.Location = new System.Drawing.Point(0, 123);
+            this.btnAsistencia.Name = "btnAsistencia";
+            this.btnAsistencia.Size = new System.Drawing.Size(188, 44);
+            this.btnAsistencia.TabIndex = 3;
+            this.btnAsistencia.Text = "Asistencia";
+            this.btnAsistencia.UseVisualStyleBackColor = false;
+            this.btnAsistencia.Click += new System.EventHandler(this.btnAsistencia_Click);
+            // 
+            // panelHeader1
+            // 
+            this.panelHeader1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(82)))), ((int)(((byte)(204)))));
+            this.panelHeader1.Controls.Add(this.btnMinimizar);
+            this.panelHeader1.Controls.Add(this.btnMaximizar);
+            this.panelHeader1.Controls.Add(this.btnClose);
+            this.panelHeader1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader1.Location = new System.Drawing.Point(203, 0);
+            this.panelHeader1.Name = "panelHeader1";
+            this.panelHeader1.Size = new System.Drawing.Size(856, 30);
+            this.panelHeader1.TabIndex = 1;
+            this.panelHeader1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelHeader1_MouseMove);
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimizar.FlatAppearance.BorderSize = 0;
+            this.btnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimizar.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimizar.Image")));
+            this.btnMinimizar.Location = new System.Drawing.Point(751, 2);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(28, 26);
+            this.btnMinimizar.TabIndex = 3;
+            this.btnMinimizar.UseVisualStyleBackColor = true;
+            this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+            // 
+            // btnMaximizar
+            // 
+            this.btnMaximizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMaximizar.FlatAppearance.BorderSize = 0;
+            this.btnMaximizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMaximizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaximizar.Image = ((System.Drawing.Image)(resources.GetObject("btnMaximizar.Image")));
+            this.btnMaximizar.Location = new System.Drawing.Point(785, 2);
+            this.btnMaximizar.Name = "btnMaximizar";
+            this.btnMaximizar.Size = new System.Drawing.Size(26, 26);
+            this.btnMaximizar.TabIndex = 3;
+            this.btnMaximizar.UseVisualStyleBackColor = true;
+            this.btnMaximizar.Click += new System.EventHandler(this.btnMaximizar_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Image = ((System.Drawing.Image)(resources.GetObject("btnClose.Image")));
+            this.btnClose.Location = new System.Drawing.Point(817, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(27, 25);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // panelHeader2
+            // 
+            this.panelHeader2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.panelHeader2.Controls.Add(this.lblNombreProfesor);
+            this.panelHeader2.Controls.Add(this.label2);
+            this.panelHeader2.Controls.Add(this.button3);
+            this.panelHeader2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader2.Location = new System.Drawing.Point(203, 30);
+            this.panelHeader2.Name = "panelHeader2";
+            this.panelHeader2.Size = new System.Drawing.Size(856, 61);
+            this.panelHeader2.TabIndex = 2;
+            this.panelHeader2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelHeader2_MouseMove);
+            // 
+            // lblNombreProfesor
+            // 
+            this.lblNombreProfesor.AutoSize = true;
+            this.lblNombreProfesor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreProfesor.Location = new System.Drawing.Point(119, 19);
+            this.lblNombreProfesor.Name = "lblNombreProfesor";
+            this.lblNombreProfesor.Size = new System.Drawing.Size(141, 20);
+            this.lblNombreProfesor.TabIndex = 7;
+            this.lblNombreProfesor.Text = "nombre profesor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Profesor: ";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(751, 15);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "LogOut";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // panelContenedor
+            // 
+            this.panelContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContenedor.Location = new System.Drawing.Point(203, 91);
+            this.panelContenedor.Name = "panelContenedor";
+            this.panelContenedor.Size = new System.Drawing.Size(856, 422);
+            this.panelContenedor.TabIndex = 3;
+            // 
+            // frmProfesor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1059, 513);
+            this.Controls.Add(this.panelContenedor);
+            this.Controls.Add(this.panelHeader2);
+            this.Controls.Add(this.panelHeader1);
+            this.Controls.Add(this.panelMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmProfesor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panelMenu.ResumeLayout(false);
+            this.panelMenu.PerformLayout();
+            this.panelHeader1.ResumeLayout(false);
+            this.panelHeader2.ResumeLayout(false);
+            this.panelHeader2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelHeader1;
+        private System.Windows.Forms.Button btnMaximizar;
+        private System.Windows.Forms.Button btnMinimizar;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Panel panelHeader2;
+        private System.Windows.Forms.Button btnAsistencia;
+        private System.Windows.Forms.Button btnNotas;
+        private System.Windows.Forms.Panel panelContenedor;
+        private UCAsistencia UCAsistencia;
+        private UCNotas UCNotas;
+        private System.Windows.Forms.Panel slideMenu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblNombreProfesor;
+    }
+}
+
