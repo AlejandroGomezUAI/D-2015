6ffa19e790d20aa3839d0fca0417f0b1cd481fb0
AlejandroGomezUAI <30306375+AlejandroGomezUAI@users.noreply.github.com>
2019-07-15 17:05:17 -0300

    Agrego idiomas al form de login

--- SIUNCA/GUI/Login.cs
+++ SIUNCA/GUI/Login.cs
@@ -10,6 +10,10 @@ using System.Windows.Forms;
 using BIZ;
 using Framework.D_2015;
 using BLL;
+//Using necesarios para el idioma
+using System.Threading;
+using System.Globalization;
+using GUI.Idiomas;
 
 namespace GUI
 {
@@ -22,6 +26,8 @@ namespace GUI
         {
             InitializeComponent();
             lblError.Visible = false;
+            cboIdioma.SelectedIndex = 0;
+            cboRol.SelectedIndex = 2;
         }
 
         private void btnMinimizar_Click(object sender, EventArgs e)
@@ -50,7 +56,7 @@ namespace GUI
         private void btnLogin_Click(object sender, EventArgs e)
         {
             var usuario = new Usuario();
-            
+
             usuario.email = txtUsuario.Text;
             usuario.password = txtContraseña.Text;
             usuario.rol = cboRol.Text;
@@ -80,7 +86,7 @@ namespace GUI
                 else
                 {
                     lblError.Visible = true;
-                    lblError.Text = "Usuario o contraseña incorrecto";
+                    //lblError.Text = "Usuario o contraseña incorrecto";
                 }
             }
         }
@@ -103,6 +109,42 @@ namespace GUI
         private void panelHeader_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void cboIdioma_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cambiarIdioma(cboIdioma.SelectedItem.ToString());
+        }
+
+        /// <summary>
+        /// El idioma por defecto es español, el archivo por defecto es Res.resx
+        /// </summary>
+        void IdiomaPorDefecto()
+        {
+            lblIdioma.Text = Res.lblIdioma;
+            lblInicioDeSesion.Text = Res.lblInicioDeSesion;
+            lblUsuario.Text = Res.lblUsuario;
+            lblContraseña.Text = Res.lblContraseña;
+            lblRol.Text = Res.lblRol;
+            lblError.Text = Res.lblError;
+            btnLogin.Text = Res.btnLogin;
+            btnCancelar.Text = Res.btnCancelar;
+        }
+
+        void cambiarIdioma(string idioma)
+        {
+            if (idioma == "Ingles")
+            {
+                //Selecciona el archivo Res.en-US.resx
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+                IdiomaPorDefecto();
+            }
+            if (idioma == "Español")
+            {
+                //Selecciona el archivo Res.resx
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("");
+                IdiomaPorDefecto();
+            }
         }
 
         /// <summary>
